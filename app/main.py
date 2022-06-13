c2d33d744f0ddea3c89d1220a138f8befd90cf04
William Pereira <willpereirabr@gmail.com>
2022-06-13 17:29:23 -0300

    using flask, same logic as previous code

--- app/main.py
+++ app/main.py
@@ -1,30 +1,11 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
-from typing import Tuple
+from datetime import datetime
+from flask import Flask, request
 
-from typing_extensions import Self
+app = Flask(__name__)
 
 
-class GSIServer(HTTPServer):
-    def __init__(self: Self, server_address: Tuple[str, int], token: str, RequestHandler) -> None:
-        super(GSIServer, self).__init__(server_address, RequestHandler)
-
-        self.auth_token = token
-        self.setup_logger()
-
-    def setup_logger(self):
-        pass
-
-class RequestHandler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        self.send_header('Content-type', 'text/html')
-        self.send_response(200)
-        self.end_headers()
-
-server = GSIServer(('localhost', 3000), 'no_password', RequestHandler)
-print('Starting server')
-
-try:
-    server.serve_forever()
-except (KeyboardInterrupt, SystemExit):
-    print('Ending server')
-    server.server_close()
+@app.route("/", methods=['POST'])
+def index() -> any:
+    date: datetime = datetime.now()
+    assert request.method == 'POST'
+    return f"Received at: {date.strftime('%H:%M:%S')}"
